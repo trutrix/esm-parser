10824fe8792d1bcb6e5631545927c6d7d84d1280
SoK <sok@monocyte.host>
2024-10-03 18:14:07 +0100

    added esm-parser bin

---
+++ src/bin/esm-parser.rs
@@ -0,0 +1,29 @@
+//! Elder Scrolls Mod format parser.
+
+use esm_parser::prelude::*;
+
+fn main() -> esm_parser::Result<()> {
+    // parse args
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: {} <file_path>", args[0]);
+        return Ok(())
+    }
+
+    // parse file using guesser
+    let mut parser = ESMParser::file(&args[1])?;
+    parser.parse_top_level(ESMParser::TES4)
+}
+
+#[cfg(test)]
+mod tests {
+    use assert_cmd::prelude::*;
+    use std::process::Command;
+
+    #[test]
+    fn zeta() {
+        let mut cmd = Command::cargo_bin("esm-parser").unwrap();
+        cmd.arg("data/Zeta.esm");
+        cmd.assert().success();
+    }
+}
